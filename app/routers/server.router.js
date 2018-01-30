777c95dc77247618c8c6e7ecb171a897d3a29d0d
Itilon <ivanov.elect@gmail.com>
2018-01-30 21:21:46 +0200

    Search and Result View, as well as Router Update

--- app/routers/server.router.js
+++ app/routers/server.router.js
@@ -8,6 +8,21 @@ const attachTo = (app, data) => {
             res.render('home');
         })
 
+        .get('/search', (req, res) => {
+            const searchTerm = req.query.search;
+
+            data.get('search/tweets', 
+                    { q: `${searchTerm} since:2018-01-30`, count: 5 }, 
+                    (err, data, response) => {
+
+                        const twitterData = data.statuses;
+
+                        res.render('search', {
+                            data: twitterData
+                        });
+                    });
+        });
+
     app.use('/', router);
 };
 
---
+++ config/app.config.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const configApp = (app) => {
+    app.set('view engine', 'pug');
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+
+    app.use('/static', express.static(
+        path.join(__dirname, '../static'))
+    );
+
+    app.use('/libs', express.static(
+        path.join(__dirname, '../node_modules'))
+    );
+};
+
+module.exports = { configApp };
